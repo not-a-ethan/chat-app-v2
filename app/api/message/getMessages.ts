384ca5d2c7f75c10974c34ef4afc650b440019cb
not-a-ethan <92686703+not-a-ethan@users.noreply.github.com>
2025-10-19 17:31:20 -0400

    Matched user PFPs to their messages when fetching

--- app/api/message/getMessages.ts
+++ app/api/message/getMessages.ts
@@ -4,6 +4,8 @@ import { getToken } from "next-auth/jwt";
 
 import { getAll } from "@/database/db";
 
+import { DatabaseMessages, DatabaseUsers } from "@/types";
+
 export async function GET(req: NextRequest) {
     const token = await getToken({ req });
     
@@ -33,12 +35,34 @@ export async function GET(req: NextRequest) {
         );
     };
 
-    const sqlMessages = await getAll(`SELECT * FROM messages WHERE roomId=$i ORDER BY id DESC LIMIT 50`, { "$i": roomId });
-    const messages = sqlMessages[0];
+    const messages: DatabaseMessages[] = await getAll(`SELECT * FROM messages WHERE roomId=$i ORDER BY id DESC LIMIT 50`, { "$i": roomId });
+
+    interface usersType {
+        [index: string]: any
+    };
+
+    const users: usersType = {};
+
+    for (let i = 0; i < messages.length; i++) {
+        if (users[messages[i]["user"]] == null) {
+            continue;
+        };
+
+        const thisUserData: DatabaseUsers[] = await getAll(`SELECT * FROM users WHERE githubId=${messages[i]["user"]}`, {});
+
+        if (thisUserData[0]["pfp"] == null) {
+            // Set a default pfp for users HERE
+
+            continue;
+        };
+
+        users[messages[i]["user"]] = thisUserData[0]["pfp"];
+    };
 
     return NextResponse.json(
         {
-            messages: messages
+            messages: messages,
+            users  : users
         },
         { status: 200 }
     );
--- types/index.ts
+++ types/index.ts
@@ -9,3 +9,24 @@ export interface AccountExists {
   usernameExists: boolean,
   idExists: boolean
 };
+
+export interface DatabaseMessages {
+  id: number,
+  roomId: number,
+  user: number,
+  content: string
+};
+
+export interface DatabaseRooms {
+  id: number,
+  name: string,
+  owner: number
+};
+
+export interface DatabaseUsers {
+  githubId: number,
+  name: string,
+  pfp: string|null,
+  lastActivity: number,
+  rooms: string
+};
