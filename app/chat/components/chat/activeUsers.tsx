3be44b199bfc656251df72a4e43b4e59b1280ad4
not-a-ethan <92686703+not-a-ethan@users.noreply.github.com>
2025-10-19 23:53:17 -0400

    Made active users display

--- app/chat/components/chat/activeUsers.tsx
+++ app/chat/components/chat/activeUsers.tsx
@@ -3,6 +3,7 @@ import { Listbox, ListboxSection, ListboxItem } from "@heroui/listbox";
 import { addToast } from "@heroui/toast";
 
 import { getAPI } from "@/helpers/getAPI";
+import { DatabaseUsers } from "@/types";
 
 export function ActiveUsers(props: any) {
     const roomID = props.room;
@@ -28,9 +29,27 @@ export function ActiveUsers(props: any) {
         };
     };
 
+    const people: DatabaseUsers[] = json["people"];
+
+    console.log(people);
+
     return (
-        <>
-            <p>Hello there</p>
-        </>
+        <Listbox classNames={{
+                base: "max-w-xs",
+                list: "max-h-[300px] overflow-scroll",
+            }}
+            items={people}
+        >
+            {people.map((person) => (
+                <ListboxItem key={person.githubId} textValue={person.name}>
+                    <div className="flex gap-2 items-center">
+                    <Avatar alt={person.name} className="shrink-0" size="sm" src={person.pfp} />
+                    <div className="flex flex-col">
+                        <span className="text-small">{person.name}</span>
+                    </div>
+                    </div>
+                </ListboxItem>
+            ))}
+        </Listbox>
     );
 };
