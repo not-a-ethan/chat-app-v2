776fe1a7352661c1673bc11f4481e20a78081615
not-a-ethan <92686703+not-a-ethan@users.noreply.github.com>
2025-10-19 22:18:54 -0400

    Endpoint to change name

---
+++ app/api/account/name/change.ts
@@ -0,0 +1,51 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { getToken } from "next-auth/jwt";
+
+import { getAll, changeDB } from "@/database/db";
+
+export async function PUT(req: NextRequest) {
+    const token = await getToken({ req });
+    
+    if (!token) {
+        return NextResponse.json(
+            {
+                "error": "Not authenticated"
+            },
+            { status: 403 }
+        );
+    };
+
+    const userId: number = Number(token.sub);
+
+    const body = await req.json();
+    const newName: string = body["name"];
+
+    if (newName.trim().length === 0) {
+        return NextResponse.json(
+            {
+                "error": "You cant be nameless"
+            },
+            { status: 400 }
+        );
+    };
+
+    const nameExists = await getAll(`SELECT * FROM users WHERE name=$n`, { "$n": newName });
+    
+    if (nameExists.length > 0) {
+        return NextResponse.json(
+            {
+                "error": "Your name must be unique"
+            },
+            { status: 400 }
+        );
+    };
+
+    const query = `UPDATE users SET name=$n WHERE githubID=${userId}`;
+    changeDB(query, { "$n": newName });
+
+    return NextResponse.json(
+        {},
+        { status: 200 }
+    );
+};
