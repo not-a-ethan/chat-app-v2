173713bd845be25c53963d7699cee533736c409e
not-a-ethan <92686703+not-a-ethan@users.noreply.github.com>
2025-10-20 22:34:43 -0400

    Auto direct users to chat page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,17 @@
+'use client';
+
+import { useEffect } from "react";
+
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/chat");
+  }, [])
+
+
   return (
     <>
       <h1>Chat App V2</h1>
