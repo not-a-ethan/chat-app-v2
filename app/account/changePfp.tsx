f5472ed663fbbe722aaa7732354a976fba8d0614
not-a-ethan <92686703+not-a-ethan@users.noreply.github.com>
2025-10-25 21:40:35 -0400

    Created Component to change URL

--- app/account/changePfp.tsx
+++ app/account/changePfp.tsx
@@ -4,5 +4,29 @@ import { Button } from "@heroui/button";
 import { addToast } from "@heroui/toast";
 
 export function ChangePfp(props: any) {
-    
+    function handleChange(e: any) {
+        e.preventDefault();
+
+        const newPfp: Blob = document.getElementById("newpfp")?.files[0];
+
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            // Data URL
+            const image = e.target?.result;
+            
+            
+        };
+
+        reader.readAsDataURL(newPfp);
+    };
+
+    return (
+        <>
+            <Form onSubmit={handleChange}>
+                <Input label="New Pfp" type="file" name="newpfp" id="newpfp" />
+
+                <Button type="submit">Change</Button>
+            </Form>
+        </>
+    );
 };
--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,5 +1,11 @@
+'use client';
+
+import { ChangePfp } from "./changePfp";
+
 import { DatabaseUsers } from "@/types";
 
 export default function Account() {
-
+    return (
+        <ChangePfp />
+    )
 };
